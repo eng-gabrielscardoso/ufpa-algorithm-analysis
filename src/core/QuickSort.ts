df7577111adcd37a5996a7a925b5289224646a7f
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-05-13 15:14:05 -0300

    feat: make quick sort pivot sortable
    Now the usage of middle element as pivot is optional and is passed
    as argument in the constructor of quick sort class, by default
    the value is false and the algorithm uses a random element from
    items passed as first argument

--- src/core/QuickSort.ts
+++ src/core/QuickSort.ts
@@ -3,32 +3,34 @@
  *
  * Quicksort is an efficient, general-purpose sorting algorithm. Was developed
  * by Tony Hoare in 1959 and published in 1961.
- * 
+ *
  * Quicksort is a divide-and-conquer algorithm. It works by selecting a 'pivot'
- * element from the array and partitioning the elements into two sub-arrays, 
+ * element from the array and partitioning the elements into two sub-arrays,
  * according to whether they are less than or greater than the pivot. For this
  * reason it is sometimes called partition-exchange sort. The sub-arrays are
  * then sorted recursively. This can be done in-place, requiring small
  * additional amounts of memory to perform the sorting.
- * 
+ *
  * Quicksort is a comparison sort, meaning that it can sort items of any type
  * for which a "less-than" relation is defined. Most implementations of
  * quicksort are not stable meaning that the relative order of equal sort
  * items is not preserved.
- * 
+ *
  * Mathematical analysis of quicksort shows that, on average, the algorithm
  * takes O (n log n) comparisons to sort n items. In the worst case it makes
  * O (n^2) comparisons.
  *
  * @source https://en.wikipedia.org/wiki/Quicksort
- * 
+ *
  * @author eng-gabrielscardoso
  */
 export default class QuickSort {
   private arr: number[];
+  private useMiddleElementAsPivot: boolean;
 
-  constructor(arr: number[]) {
+  constructor(arr: number[], useMiddleElementAsPivot = false) {
     this.arr = arr;
+    this.useMiddleElementAsPivot = useMiddleElementAsPivot;
   }
 
   public sort(): number[] {
@@ -36,13 +38,20 @@ export default class QuickSort {
       return this.arr;
     }
 
-    const middleIndex = Math.floor(this.arr.length / 2);
-    const pivot = this.arr[middleIndex];
+    let pivotIndex: number;
+
+    if (this.useMiddleElementAsPivot) {
+      pivotIndex = Math.floor(this.arr.length / 2);
+    } else {
+      pivotIndex = Math.floor(Math.random() * this.arr.length);
+    }
+
+    const pivot = this.arr[pivotIndex];
     const left = [];
     const right = [];
 
     for (let i = 0; i < this.arr.length; i++) {
-      if (i === middleIndex) {
+      if (i === pivotIndex) {
         continue;
       }
 
@@ -53,8 +62,11 @@ export default class QuickSort {
       }
     }
 
-    const sortedLeft = new QuickSort(left).sort();
-    const sortedRight = new QuickSort(right).sort();
+    const sortedLeft = new QuickSort(left, this.useMiddleElementAsPivot).sort();
+    const sortedRight = new QuickSort(
+      right,
+      this.useMiddleElementAsPivot
+    ).sort();
 
     return [...sortedLeft, pivot, ...sortedRight];
   }
