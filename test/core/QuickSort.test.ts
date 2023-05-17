0543a52c4a00b0a0a08069a556520e141729ef12
Gabriel Santos Cardoso <67348001+eng-gabrielscardoso@users.noreply.github.com>
2023-05-17 21:50:45 +0000

    feat: solve requirement 1.1

--- test/core/QuickSort.test.ts
+++ test/core/QuickSort.test.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import QuickSort from "src/core/QuickSort";
 
 describe("QuickSort tests", () => {
@@ -8,5 +9,18 @@ describe("QuickSort tests", () => {
     const qs = new QuickSort(numbers);
 
     expect(qs).not.toBeNull();
+  });
+
+  /** @test */
+  test("should order a random sequence of 1-100 range as input and print the result of execution", () => {
+    const numbers = new Array(100)
+      .fill(0)
+      .map((element) => (element = faker.number.int({ min: 1, max: 100 })));
+
+    const qs = new QuickSort(numbers, true);
+
+    expect(qs.sort()).toStrictEqual(
+      numbers.sort((a: number, b: number): number => a - b)
+    );
   });
 });
