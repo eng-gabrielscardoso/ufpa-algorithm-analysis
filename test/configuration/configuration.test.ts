93591cadfcfef17ca2715f85bcaa3f9a17c49eb3
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-05-21 23:49:50 -0300

    test: adjust configuration tests

--- test/configuration/configuration.test.ts
+++ test/configuration/configuration.test.ts
@@ -3,10 +3,13 @@ import configuration from "src/config/configuration";
 
 dotenv.config();
 
-describe("Configuration file tests", async () => {
+describe("Configuration file tests", () => {
   /** @test */
-  test("should load correctly API base URL", () => {
+  test("should load correctly API base URL", async () => {
+    const appEnv = process.env.APP_ENV;
     const apiBaseUrl = process.env.API_BASE_URL;
+
+    expect(configuration.appEnv).toEqual(appEnv);
     expect(configuration.apiBaseUrl).toEqual(apiBaseUrl);
   });
 });
